f8893a6509849f14d3d76f9f7d8b1c06c5186f09
Alan <zeyu.weng@gmail.com>
2020-11-25 15:41:17 -0500

    Home controller now has comments and the table will keep expanding depending on the number of habits

--- Habit Tracker/Controller/HomeController.swift
+++ Habit Tracker/Controller/HomeController.swift
@@ -10,7 +10,8 @@ import UIKit
 class HomeController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
-    var userGoal: String?
+    var userHabitsList: [String] = []
+    var userHabitName: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,25 +25,29 @@ class HomeController: UIViewController, UITableViewDataSource, UITableViewDelega
         present(goalVC, animated: true, completion: nil)
     }
     
+    // Think of how ios settings have different sections w diff spacings
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
+    // Allows the table to keep expanding based on how many habits are in the array
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return userHabitsList.count
     }
     
+    // Actually puts the words in the table
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "habitName")!
-        print("im being called")
-        cell.textLabel?.text = userGoal ?? ""
+        // indexPath.row allows the cell to use the most recent added habit
+        cell.textLabel?.text = userHabitsList[indexPath.row]
         return cell
     }
 }
 
 extension HomeController: CreateGoalDelegate {
     func didTapSave(name: String) {
-        userGoal = name
+        userHabitName = name
+        userHabitsList.append(name)
         tableView.reloadData()
     }
 }
