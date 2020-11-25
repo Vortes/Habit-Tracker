69bbb89e4ebc6398dbd7af94d3bfa77b43786736
Alan <zeyu.weng@gmail.com>
2020-11-24 21:26:54 -0500

    segue skeleton done

---
+++ Habit Tracker/NewGoalViewController.swift
@@ -0,0 +1,22 @@
+//
+//  NewGoalViewController.swift
+//  Habit Tracker
+//
+//  Created by Alan Weng on 11/24/20.
+//
+
+import UIKit
+
+class NewGoalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func Save(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
--- Habit Tracker/ViewController.swift
+++ Habit Tracker/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func createNewGoal(_ sender: Any) {
+        self.performSegue(withIdentifier: "goToCreateView", sender: self)
+    }
+    
 }
 
