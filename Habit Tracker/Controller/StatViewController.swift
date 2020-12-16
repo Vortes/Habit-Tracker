907287aafb4f1183d8a14f0be86386ffdd064bb1
Alan <zeyu.weng@gmail.com>
2020-12-16 15:50:22 -0500

    Starting work on stat view controller

---
+++ Habit Tracker/Controller/StatViewController.swift
@@ -0,0 +1,22 @@
+//
+//  StatViewController.swift
+//  Habit Tracker
+//
+//  Created by Alan Weng on 12/16/20.
+//
+
+import UIKit
+
+class StatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+	@IBAction func cancelButton(_ sender: Any) {
+		dismiss(animated: true, completion: nil)
+	}
+
+}
