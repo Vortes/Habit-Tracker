c31d900bb46d5ec4b2b515658dcec66723bde558
Alan <zeyu.weng@gmail.com>
2020-12-19 15:17:52 -0500

    Fixed auto layout issue in createHabit

---
+++ Habit Tracker/View/HabitCell.swift
@@ -0,0 +1,23 @@
+//
+//  HabitCell.swift
+//  Habit Tracker
+//
+//  Created by Alan Weng on 12/19/20.
+//
+
+import UIKit
+
+class HabitCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
